d5b959d46b233949f7cf8f799e35ad4e1bb14edf
Tayse <tayse_rosa@yahoo.com.br>
2021-12-11 06:57:40 -0300

    Tipando e acessando propriedades no Styled Components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -35,9 +35,26 @@ export function Dashboard(){
       </Header>
 
       <HighlightCards >
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard 
+          type="up"
+          title="Entradas" 
+          amount="R$17.400,00" 
+          lastTransaction="Última entrada dia 13 de Abril de 2021" 
+        />
+
+        <HighlightCard 
+          type="down"
+          title="Saídas" 
+          amount="R$1.259 ,00" 
+          lastTransaction="Última entrada dia 03 de Abril de 2021" 
+        />
+
+        <HighlightCard 
+          type="total"
+          title="Total" 
+          amount="R$16.141,00" 
+          lastTransaction="01 á 16 de Abril de 2021" 
+        />
       </HighlightCards>
     </Container>
   )
