6ca4a916e86f3e59fb5cc128085281afc70d7a70
Tayse <tayse_rosa@yahoo.com.br>
2021-12-20 16:22:21 -0300

    Criando a interface de grafico

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,6 +8,7 @@ const { Navigator, Screen } = createBottomTabNavigator();
 //Telas
 import { Dashboard } from "../screens/Dashboard";
 import { Register } from "../screens/Register";
+import { Resume } from "../screens/Resume";
 
 
 export function AppRoutes(){
@@ -60,7 +61,7 @@ export function AppRoutes(){
 
       <Screen 
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{ 
           tabBarIcon: (({ size, color }) =>
             <MaterialIcons 
---
+++ src/screens/Resume/index.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+//Component
+import { HistoryCard } from '../../components/HistoryCard';
+
+import {
+  Container,
+  Header,
+  Title
+} from './styles';
+
+export function Resume(){
+  return(
+    <Container>
+      <Header>
+        <Title> Resumo por categoria </Title>
+      </Header>
+
+      <HistoryCard 
+        title="Compras"
+        amount="R$ 150,50"
+        color="red"
+      />
+      
+    </Container>
+  )
+}
